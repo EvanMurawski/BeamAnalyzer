2196cfbf82635d9f00ef73f59351428eb3c9ac2a
Evan Murawski <evan.murawski@gmail.com>
2014-07-23 20:31:54 -0400

    updated makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: guistructure.ui
 	pyuic4 guistructure.ui > guistructure.py
 	pyuic4 forcemomentprompt.ui > forcemomentprompt.py
-
+	pyuic4 distforceprompt.ui > distforceprompt.py
--- guistructure.py
+++ guistructure.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'guistructure.ui'
 #
-# Created: Sun Jul 20 20:20:13 2014
+# Created: Wed Jul 23 20:26:03 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
